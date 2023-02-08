3411f11ca2e76d1df7fdf2ea19ce0c23ab486f07
Dan Boitnott <dboitnot@gmail.com>
2023-02-08 11:06:08 -0600

    Removed old test

--- src/auth/policy_tests.rs
+++ src/auth/policy_tests.rs
@@ -2,28 +2,47 @@
 mod policy_tests {
     use super::super::*;
 
-    struct TestContext {
-        empty_user: User,
-    }
+    // struct TestContext {
+    //     empty_user: User,
+    // }
 
-    impl PolicyStore for TestContext {
-        fn group_named(&self, name: &str) -> Option<&Group> {
-            match name {
-                _ => None,
-            }
-        }
-    }
+    // impl PolicyStore for TestContext {
+    //     fn list_users(&self) -> Result<Vec<User>, ()> {
+    //         todo!()
+    //     }
+    //     fn create_user(&self, user: &User) -> Result<(), ()> {
+    //         todo!()
+    //     }
+    //     fn update_user(&self, user: &User) -> Result<(), ()> {
+    //         todo!()
+    //     }
+    //     fn set_user_password(&self, login_name: &str, password: Option<&str>) -> Result<(), ()> {
+    //         todo!()
+    //     }
+    //     fn authenticate_user(&self, login_name: &str, password: &str) -> Result<User, ()> {
+    //         todo!()
+    //     }
+    //     fn user_named(&self, name: &str) -> Result<User, ()> {
+    //         todo!()
+    //     }
+    //     fn list_groups(&self) -> Result<Vec<Group>, ()> {
+    //         todo!()
+    //     }
+    //     fn group_named(&self, name: &str) -> Option<Group> {
+    //         todo!()
+    //     }
+    // }
 
-    fn setup() -> TestContext {
-        TestContext {
-            empty_user: User {
-                login_name: String::from("username"),
-                full_name: None,
-                groups: Vec::new(),
-                policy_statements: Vec::new(),
-            },
-        }
-    }
+    // fn setup() -> TestContext {
+    //     TestContext {
+    //         empty_user: User {
+    //             login_name: String::from("username"),
+    //             full_name: None,
+    //             groups: Vec::new(),
+    //             policy_statements: Vec::new(),
+    //         },
+    //     }
+    // }
 
     // #[test]
     // fn test_user_may_perform_empty_denies() {
